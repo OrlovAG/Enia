702a6fcb4e073ffa72683c1bb6bbd349a997db8c
OrlovAG <orlovkolpino@gmail.com>
2024-09-25 11:21:29 +0300

    +input

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/INPUT.txt"));
 
         int N, A, B;
         N = scanner.nextInt();
